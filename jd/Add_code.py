c241912537ccb6d6d36715bfdc4a8a43d374bb89
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-01 10:33:25 +0800

    Update Add_code.py

--- jd/Add_code.py
+++ jd/Add_code.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-zdUrl = 'http://api.turinglabs.net/api/v1/jd/bean/create/'
-ncUrl = 'http://api.turinglabs.net/api/v1/jd/farm/create/'
-mcUrl = 'http://api.turinglabs.net/api/v1/jd/pet/create/'
+zdUrl = 'http://api.turinglabs.net/api/v1/jd/bean/create/' #种豆 
+ncUrl = 'http://api.turinglabs.net/api/v1/jd/farm/create/' #农场
+mcUrl = 'http://api.turinglabs.net/api/v1/jd/pet/create/'  #萌宠
+ddUrl = 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/' #东东工厂
+jxUrl = 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/' #京喜工厂
 
-Defalt_ncShareCode= ['ae6488dc5f0c4669bfa432b9bc884191','268e797816f340bc9ad3656fa249d1a6','010bcbf67bf54e7cae65b00d252f462c','b2e08a4105674c6680a3ba687627e9f2','34e1ac7416f34d1e86ebffb1f9f1ce72']#读取参数djj_sharecode为空，开始读取默认参数
-Defalt_mcShareCode= ['MTAxODExNDYxMTEwMDAwMDAwMzk4NzYxOTc=','MTAxODExNDYxMTEwMDAwMDAwNDA1NDQwNzE=','MTAxODc2NTEzMzAwMDAwMDAyODk1OTQ4OQ==','MTAxODc2NTEzOTAwMDAwMDAyODY1NzEzMw==']#读取参数djj_sharecode为空，开始读取默认参数
-Defalt_zdShareCode= ['7oivz2mjbmnx4bddymkpj42u75jmba2c6ga6eba','2vgtxj43q3jqzr2i5ac4uj2h6wxl66n4i326u3i','4npkonnsy7xi2rt6qcajwzdp3qjmazppr5fwo6a','fsvcjluk7rhd74kwgrelr3aoeu','olmijoxgmjutykb27yfwpryuu5cot5fj3frld5q']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_ncShareCode= ['535587d2d2134c05b72d532e5648fca4','d8f30abd6b09497b81502e328e6822ae','c777008122e34854b7bf243d9027b0b3']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_mcShareCode= ['MTAxODc2NTEzMTAwMDAwMDAyNjg3ODA3Nw==','MTAxODc2NTEzMTAwMDAwMDAyNzM1OTgyOQ==','MTE1NDAxNzYwMDAwMDAwNDA0NjI0MDk=']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_zdShareCode= ['2gih437f3cx52vug2oxzjdhkoa','eeexxudqtlampttwqsjgtjinenirfsmkhcanvfq','uwgpfl3hsfqp3onctpv6qufih5slnir6rura7fy']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_ddShareCode= ['P04z54XCjVWnYaS5mRUVDWmnD1Ll4CY','P04z54XCjVWnYaS5m9cZxqYhAUtywhKqBmvOA','P04z54XCjVWnYaS5m9cZz6DtAs01UPsoL1lIQ']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_jxShareCode= ['Z-OXyhGaVPX6ImRJwOb_pQ==']#读取参数djj_sharecode为空，开始读取默认参数
 #学习与测试  by 2020.12
 def AddhelpCode(Url,Defalt_ShareCode):
    for code in Defalt_ShareCode:
@@ -32,3 +36,5 @@ def hongliyu(url):
 AddhelpCode(mcUrl,Defalt_mcShareCode)
 AddhelpCode(ncUrl,Defalt_ncShareCode)
 AddhelpCode(zdUrl,Defalt_zdShareCode)
+AddhelpCode(ddUrl,Defalt_ddShareCode)
+AddhelpCode(jxUrl,Defalt_jxShareCode)
