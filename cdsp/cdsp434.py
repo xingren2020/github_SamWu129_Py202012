b25b2f6796129a1208fc785a7b85db7e06078863
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-09 16:40:26 +0800

    Update cdsp434.py

--- cdsp/cdsp434.py
+++ cdsp/cdsp434.py
@@ -13,193 +13,26 @@ from dateutil import tz
 result=''
 djj_bark_cookie=''
 djj_sever_jiang=''
-qq_read_cookie=''
+cd_tk=''
+cd_token=''
+cd_body434=''
 
+tklist= []
+tokenlist=[]
 
+headers={"Accept": "*/*","Accept-Encoding": "br, gzip, deflate","Accept-Language": "zh-Hans-CN;q=1, en-US;q=0.9, zh-Hant-CN;q=0.8","As-Version": "v1","Content-Type": "application/json","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 qapp","Version": "1211","Version-Name": ""}
 
-cooklist=[]
-headers={'User-Agent': 'QQ/8.4.10.666 CFNetwork/978.0.7 Darwin/18.7.0','Content-Type':'application/json'}
-def user_name():
-   msg='【nickname】'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/user/init',headers=headers,timeout=10)
-      #print(response.text)
-      userRes=json.loads(response.text)
-      if(userRes['code']==0):
-         msg+=f'''{userRes['data']['user']['nickName']}'''
-      else:
-       	msg+=f'''{userRes['msg']}'''
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def user_coin():
-   msg='【coin】'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/page?fromGuid=',headers=headers,timeout=10)
-      #print(response.text)
-      userRes=json.loads(response.text)
-      if(userRes['code']==0):
-         msg+=f'''💰{userRes['data']['user']['amount']} '''
-      else:
-       	msg+=f'''{userRes['msg']}'''
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def user_readtime():
-   msg='【readtime】'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/me/query/page',headers=headers,timeout=10)
-      #print(response.text)
-      userRes=json.loads(response.text)
-      if(userRes['code']==0):
-         msg+=f'''💰{userRes['data']['readTime']}  min  【Readmoney】{userRes['data']['balance']['allBalance']}bean'''
-      else:
-       	msg+=f'''{userRes['msg']}'''
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
 def watch_video():
    msg='watch_video'
    print('\n💎'+msg)
    try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/watch_video',headers=headers,timeout=10)
+      response=requests.get('https://api-ddvideo.1sapp.com/h5/task/submit',headers=headers,body=cd_body434,timeout=10)
       #print(response.text)
    except Exception as e:
       msg+=str(e)
       print(msg)
    loger(msg)
       
-def treasure_box():
-   msg='treasure_box'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/treasure_box',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-      
-def treasure_box_video():
-   msg='treasure_box_video'
-   time.sleep(31)
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/treasure_box_video',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-      
-def clock_in():
-   msg='clock_in'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/clock_in/page',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def clock_in_video():
-   msg='clock_in_video'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/clock_in_video',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-      
-def read_red_day():
-   msg='read_red_day'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/red_packet/user/read_book',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def read_time(long):
-   msg='read_time'
-   print('\n💎'+msg)
-   try:
-      response=requests.get(f'''https://mqqapi.reader.qq.com/mqq/red_packet/user/read_time?seconds={long}''',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def read_time_reward(long):
-   msg='read_time_reward'
-   print('\n💎'+msg)
-   try:
-      response=requests.get(f'''https://mqqapi.reader.qq.com/mqq/red_packet/user/read_time_reward?seconds={long}''',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def read_time_long():
-   msg='read_time_long'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/addReadTimeWithBid?scene=1008&refer=pages%2Fbook-shelf%2Findex&bid=27693007&readTime=188447&read_type=0&conttype=1&read_status=0&chapter_info=%5B%7B%222%22%3A%7B%22readTime%22%3A188447%2C%22pay_status%22%3A0%7D%7D%5D&sp=-1',headers=headers,timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-   
-def readtimecheck():
-   msg='readtimecheck\n'
-   print('\n💎'+msg)
-   try:
-      response=requests.get('https://mqqapi.reader.qq.com/mqq/page/config?router=%2Fpages%2Fbook-read%2Findex&options=%7B%22bid%22%3A%2227693007%22%2C%22cid%22%3A%222%22%2C%22from%22%3A%22shelf%22%7D',headers=headers,timeout=10)
-      #print(response.text)
-      rtm=json.loads(response.text)
-      if(rtm['code']==0 and rtm['msg']=='ok'):
-        msg+=f'''todayReadSeconds:{rtm['data']['pageParams']['todayReadSeconds']}'''
-        for item in rtm['data']['pageParams']['readTimeRewardTask']:
-            if item['enableFlag']==1 and item['doneFlag']==0:
-              read_time_reward(item['seconds'])
-        for item in rtm['data']['pageParams']['readTimeTask']:
-            if item['enableFlag']==1 and item['doneFlag']==0:
-              read_time(item['seconds'])
-      else:
-          	msg+='err'
-
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
-
-def sign_in():
-   msg='sign_in'
-   print('\n💎'+msg)
-   try:
-      response=requests.post('https://mqqapi.reader.qq.com/mqq/sign_in/user',headers=headers,data={},timeout=10)
-      #print(response.text)
-   except Exception as e:
-      msg+=str(e)
-      print(msg)
-   loger(msg)
    
 def clock(func):
     def clocked(*args, **kwargs):
@@ -243,26 +76,20 @@ def check(st,flag,list):
    else:
        print('DTask is over.')
        exit()
+	
 def all():
-   check(qq_read_cookie,'QQ_READ_COOKIE',cooklist)
+   check(cd_tk,'CD_TK',tklist)
+   check(cd_token,'CD_TOKEN',tokenlist)
+   check(cd_body434,'CD_BODY434')
    index=0
    global result
-   for item in cooklist:
-       headers['Cookie']=item
+   for item in tklist:
+       headers['Tk']=item
+   for item in tokenlist:
+       headers['Token']=item
        index+=1
        result+=f'''【Count】{index}'''
-       user_name()
-       user_coin()
        watch_video()
-       user_readtime()
-       clock_in()
-       clock_in_video()
-       read_time_long()
-       treasure_box()
-       treasure_box_video()
-       read_red_day()
-       readtimecheck()
-       sign_in()
    if notice('4:00','5:00') or notice('22:00','23:00') or notice('13:00','14:00'):
       pushmsg('QQread',result)
    print('its over')
