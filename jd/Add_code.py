c93836d825225cb19614cc2475a53404c9b4065d
SamQ <44307655+SamWu129@users.noreply.github.com>
2020-12-15 09:18:25 +0800

    Update Add_code.py

--- jd/Add_code.py
+++ jd/Add_code.py
@@ -7,10 +7,10 @@ mcUrl = 'http://api.turinglabs.net/api/v1/jd/pet/create/'  #萌宠
 ddUrl = 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/' #东东工厂
 jxUrl = 'http://api.turinglabs.net/api/v1/jd/jxfactory/create/' #京喜工厂
 
-Defalt_ncShareCode= ['535587d2d2134c05b72d532e5648fca4','d8f30abd6b09497b81502e328e6822ae','c777008122e34854b7bf243d9027b0b3']#读取参数djj_sharecode为空，开始读取默认参数
-Defalt_mcShareCode= ['MTAxODc2NTEzMTAwMDAwMDAyNjg3ODA3Nw==','MTAxODc2NTEzMTAwMDAwMDAyNzM1OTgyOQ==','MTE1NDAxNzYwMDAwMDAwNDA0NjI0MDk=']#读取参数djj_sharecode为空，开始读取默认参数
-Defalt_zdShareCode= ['2gih437f3cx52vug2oxzjdhkoa','eeexxudqtlampttwqsjgtjinenirfsmkhcanvfq','uwgpfl3hsfqp3onctpv6qufih5slnir6rura7fy']#读取参数djj_sharecode为空，开始读取默认参数
-Defalt_ddShareCode= ['P04z54XCjVWnYaS5mRUVDWmnD1Ll4CY','P04z54XCjVWnYaS5m9cZxqYhAUtywhKqBmvOA','P04z54XCjVWnYaS5m9cZz6DtAs01UPsoL1lIQ']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_ncShareCode= ['535587d2d2134c05b72d532e5648fca4','d8f30abd6b09497b81502e328e6822ae','c777008122e34854b7bf243d9027b0b3','6373073b2a774b85b5844ce1b6d9d94b']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_mcShareCode= ['MTAxODc2NTEzMTAwMDAwMDAyNjg3ODA3Nw==','MTAxODc2NTEzMTAwMDAwMDAyNzM1OTgyOQ==','MTE1NDAxNzYwMDAwMDAwNDA0NjI0MDk=','MTAxODc2NTEzMTAwMDAwMDAyMTE1NzkyNw==']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_zdShareCode= ['2gih437f3cx52vug2oxzjdhkoa','eeexxudqtlampttwqsjgtjinenirfsmkhcanvfq','uwgpfl3hsfqp3onctpv6qufih5slnir6rura7fy','4npkonnsy7xi2eincnwavowy6ohblizjt5go7ra']#读取参数djj_sharecode为空，开始读取默认参数
+Defalt_ddShareCode= ['P04z54XCjVWnYaS5mRUVDWmnD1Ll4CY','P04z54XCjVWnYaS5m9cZxqYhAUtywhKqBmvOA','P04z54XCjVWnYaS5m9cZz6DtAs01UPsoL1lIQ','P04z54XCjVWnYaS5m9cZ2f42y4Yl-YMYPx33g8']#读取参数djj_sharecode为空，开始读取默认参数
 Defalt_jxShareCode= ['Z-OXyhGaVPX6ImRJwOb_pQ==','CIAftU-mPs00k7FnlPTLgg==']#读取参数djj_sharecode为空，开始读取默认参数
 #学习与测试  by 2020.12
 def AddhelpCode(Url,Defalt_ShareCode):
